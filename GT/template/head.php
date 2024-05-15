d631ba176165e7cc726e0afc97b5445080191b16
MatiasOtamendi <106671131+matiasotamendimoreno@users.noreply.github.com>
2024-05-15 11:36:08 -0300

    Cambia el icono de XAMPP a IBM

--- GT/template/head.php
+++ GT/template/head.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Generación T: Matías Otamendi Moreno</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/slate/bootstrap.min.css">
+  <link rel="icon" href="img/IMB.ico" type="image/x-icon">
 </head>
 
 <body>
